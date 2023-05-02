dffd0e67edbcbfd3c61f128b2ec574429e81f49d
Andrew Branch <awbranch@gmail.com>
2023-05-02 17:52:07 -0400

    feat: bootstrap sanity studio

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '5gu47m8q',
+    dataset: 'production'
+  }
+})
---
+++ sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Shill Cares',
+
+  projectId: '5gu47m8q',
+  dataset: 'production',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
